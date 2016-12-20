9fbae72cc26a976698c076c8e04ab24ef690a884
Mauricio Klein <mauricio.klein@jura-online.de>
2016-12-20 12:30:46 +0100

    Test cases refactored

--- spec/english_number/english_number_spec.rb
+++ spec/english_number/english_number_spec.rb
@@ -1,44 +1,27 @@
 require 'spec_helper'
+require_relative 'test_cases'
 
 describe EnglishNumber do
-  context 'non-regular positive numbers' do
+  context 'positive numbers' do
     it do
-      (0..19).each do |number|
-        expect(EnglishNumber.new(number).to_english).to eq(English::Number::Mapping::BASE_NUMBERS[number])
+      TEST_CASES.each do |number, expected_translation|
+        expect(EnglishNumber.new(number).to_english).to eq(expected_translation)
       end
     end
   end
 
-  context 'non-regular negative numbers' do
+  context 'negative numbers' do
     it do
-      (-19..-1).each do |number|
-        expect(EnglishNumber.new(number).to_english).to eq("minus #{English::Number::Mapping::BASE_NUMBERS[number.abs]}")
+      TEST_CASES.each do |number, expected_translation|
+        expect(EnglishNumber.new(-number).to_english).to eq("minus #{expected_translation}")
       end
     end
   end
 
-  context 'regular positive numbers' do
+  context 'special cases' do
     it do
-      (20..99).each do |number|
-        dozens = number / 10
-        units = number - dozens * 10
-
-        dozens_string = English::Number::Mapping::DOZENS[dozens]
-        units_string = units > 0 ? "-#{English::Number::Mapping::BASE_NUMBERS[units]}" : nil
-        expect(EnglishNumber.new(number).to_english).to eq("#{dozens_string}#{units_string}")
-      end
-    end
-  end
-
-  context 'regular negative numbers' do
-    it do
-      (-99..-20).each do |number|
-        dozens = number.abs / 10
-        units = number.abs - dozens * 10
-
-        dozens_string = English::Number::Mapping::DOZENS[dozens]
-        units_string = units > 0 ? "-#{English::Number::Mapping::BASE_NUMBERS[units]}" : nil
-        expect(EnglishNumber.new(number).to_english).to eq("minus #{dozens_string}#{units_string}")
+      SPECIAL_TEST_CASES.each do |number, expected_translation|
+        expect(EnglishNumber.new(number).to_english).to eq(expected_translation)
       end
     end
   end
